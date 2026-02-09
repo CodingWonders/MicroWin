e1305a1c49d887afdfcd384df85dc57e5dcb32c0
Real-MullaC <callumjanes2007new+github@gmail.com>
2026-02-09 15:51:17 +0000

    Reorganize using directives and improve code structure

--- MicroWin/WizardPages.cs
+++ MicroWin/WizardPages.cs
@@ -301,7 +301,7 @@ namespace MicroWin
                 RegistryHelper.AddRegistryItem("HKLM\\zSOFTWARE\\Microsoft\\Active Setup\\Installed Components\\CMP_NoFla");
                 RegistryHelper.AddRegistryItem("HKLM\\zSOFTWARE\\Microsoft\\Active Setup\\Installed Components\\CMP_NoFla", new RegistryItem("", ValueKind.REG_SZ, "Stop First Logon Animation Process"));
                 // CW Please look at the one below as unsure if this is correct. 
-                RegistryHelper.AddRegistryItem("HKLM\\zSOFTWARE\\Microsoft\\Active Setup\\Installed Components\\CMP_NoFla", new RegistryItem("StubPath", ValueKind.REG_SZ, "\'%WINDIR%\\System32\\cmd.exe\\' /C \\'taskkill /f /im firstlogonanim.exe\\'"));
+                RegistryHelper.AddRegistryItem("HKLM\\zSOFTWARE\\Microsoft\\Active Setup\\Installed Components\\CMP_NoFla", new RegistryItem("StubPath", ValueKind.REG_SZ, "\'%WINDIR%\\System32\\cmd.exe\\' /C \\'taskkill /f /im firstlogonanim.exe\'"));
 
                 RegistryHelper.UnloadRegistryHive("HKLM\\zSYSTEM");
                 RegistryHelper.UnloadRegistryHive("HKLM\\zSOFTWARE");
@@ -345,4 +345,5 @@ namespace MicroWin
             this.Controls.AddRange(new Control[] { lbl, btnClose });
         }
     }
+
 }
