1d3c6052763ea0a3a05c5f25e772858ae4527e54
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-16 10:16:25 +0200

    Update FirstStartup.ps1

--- MicroWin/tools/FirstStartup.ps1
+++ MicroWin/tools/FirstStartup.ps1
@@ -24,7 +24,7 @@ if ((Get-BitLockerVolume -MountPoint $Env:SystemDrive).ProtectionStatus -eq 'On'
     # Check if the number of occurrences of "path" is 2 - this fixes the Boot Manager screen issue (#2562)
 if ((bcdedit | Select-String "path").Count -eq 2) {
     # Set bootmgr timeout to 0
-    bcdedit /set  `{bootmgr`} timeout 0
+    bcdedit /set `{bootmgr`} timeout 0
 }
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.Suggested" /f
@@ -79,4 +79,5 @@ $timeout = 100
 
 # Send the broadcast message to all windows
 [Win32]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [IntPtr]::Zero, "ImmersiveColorSet", $SMTO_ABORTIFHUNG, $timeout, [ref]([IntPtr]::Zero))
+
 
