148ef7029b49d305efa20437a66fec05ff62c801
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-06-11 15:08:10 +0200

    Download script now uses EXEs

--- Microwin.ps1
+++ Microwin.ps1
@@ -1,7 +1,7 @@
 [Net.ServicePointManager]::SecurityProtocol = "Tls12"
 
 $tempPath = [System.IO.Path]::GetTempPath()
-$zipFile = Join-Path $tempPath "MicroWinDownload.zip"
+$exeFile = Join-Path $tempPath "MicroWin.exe"
 $extractPath = Join-Path $tempPath "MicroWinExtract"
 
 $moduleRequest = Invoke-WebRequest -UseBasicParsing "https://api.github.com/repos/CodingWonders/MicroWin/releases/latest"
@@ -15,22 +15,23 @@ $tempDir = [IO.Path]::GetTempPath().TrimEnd("\")
 
 # Status Code 200 == OK
 $assetData = $moduleRequest.Content | ConvertFrom-Json
-Write-Host "Downloading release $($assetData.name) (tag $($assetData.tag_name))..."
 
 $downloadUri = ""
+$size = 0
 
 if ([Environment]::Is64BitOperatingSystem) {
     $downloadUri = ($assetData.assets | Where-Object { $_.browser_download_url.Contains("x64") }).browser_download_url
+    $size = ($assetData.assets | Where-Object { $_.browser_download_url.Contains("x64") }).size
 } else {
     $downloadUri = ($assetData.assets | Where-Object { $_.browser_download_url.Contains("x86") }).browser_download_url
+    $size = ($assetData.assets | Where-Object { $_.browser_download_url.Contains("x86") }).size
 }
+
+Write-Host "Downloading release $($assetData.name) ($([Math]::Round($size / 1MB, 2)) MB)..."
 
 # To get rid of long delay times we will not show any progress output
 $ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri "$downloadUri" -OutFile $zipFile
+Invoke-WebRequest -Uri "$downloadUri" -OutFile $exeFile
 $ProgressPreference = 'Continue'
 
-Expand-Archive -Path $zipFile -DestinationPath $extractPath -Force
-Remove-Item $zipFile
-
-Start-Process -FilePath (Join-Path $extractPath "MicroWin.exe")
+Start-Process -FilePath (Join-Path $tempPath "MicroWin.exe")
