be6cbbc2693d04f734de6021149798effc19110f
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-06-11 14:53:51 +0200

    Show resulting file hashes and organize target files

--- MicroWin/publish.bat
+++ MicroWin/publish.bat
@@ -36,8 +36,15 @@ echo Publishing self-contained binaries...
 for %%a in (win-x86 win-x64 win-arm64) do (
 	echo ------------------------ Building self-contained binary for target %%a ------------------------
 	dotnet publish -r:%%a -p:PublishSingleFile=true --sc -o "bin\release\net10.0-windows\sc\%%a" --framework net10.0-windows
+	if !ERRORLEVEL! equ 0 (
+		REM give the final EXE its final name
+		if /I "%%a" == "win-x86" copy "bin\release\net10.0-windows\sc\%%a\MicroWin.exe" "bin\release\net10.0-windows\sc\MicroWin_x86.exe"
+		if /I "%%a" == "win-x64" copy "bin\release\net10.0-windows\sc\%%a\MicroWin.exe" "bin\release\net10.0-windows\sc\MicroWin_x64.exe"
+		if /I "%%a" == "win-arm64" copy "bin\release\net10.0-windows\sc\%%a\MicroWin.exe" "bin\release\net10.0-windows\sc\MicroWin_arm64.exe"
+	)
 )
 
 echo Completed.
 ENDLOCAL
+powershell -noprofile -nologo -command Get-FileHash "bin\release\net10.0-windows\sc\*.exe" ^| Select-Object Hash, @{Name='FileName'; Expression={[IO.Path]^:^:GetFileName($_.Path)}}
 exit /b
