c1d1fa19ff2a4a07b7add9a5a208fc2b5ac74a04
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-08-19 21:09:30 +0200

    Add 26H2 version constant

--- MicroWin/functions/Helpers/PropertyCheckers/VersionComparer.cs
+++ MicroWin/functions/Helpers/PropertyCheckers/VersionComparer.cs
@@ -36,6 +36,10 @@ namespace MicroWin.functions.Helpers.PropertyCheckers
         /// Version constant for the GA release of Windows 11 26H1.
         /// </summary>
         public static readonly Version VERCONST_WIN11_26H1 = new(10, 0, 28000, 1575);
+        /// <summary>
+        /// Version constant for Windows 11 26H2 as of 19 August 2026.
+        /// </summary>
+        public static readonly Version VERCONST_WIN11_26H2 = new(10, 0, 26300, 9212);
 
         /// <summary>
         /// Compares versions to determine if the source version is greater than or equal to the minimum threshold.
